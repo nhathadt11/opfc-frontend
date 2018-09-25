81b642321650f6b833f6717edd910337a63de3a4
Nhat Ha <nhathadt11@gmail.com>
2018-09-25 11:36:49 +0700

    Mock loading menu card

--- src/containers/MenuCardGrid/MenuCardGrid.js
+++ src/containers/MenuCardGrid/MenuCardGrid.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { map } from 'lodash';
 import { Row, Col } from 'antd';
 import MenuCard from '../../components/MenuCard/MenuCard';
 
-const MenuCardGrid = () => (
-  <Row type="flex" gutter={24} style={{ margin: 'auto 24px' }}>
-    {
-      map(Array.from(Array(12)), (_, index) => <Col style={{ margin: '12px 0' }}><MenuCard key={index} /></Col>)
-    }
-  </Row>
-);
+class MenuCardGrid extends Component {
+  state = {
+    loading: true,
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <Row type="flex" gutter={24} style={{ margin: 'auto 24px' }}>
+        {
+          map(Array.from(Array(12)), (_, index) => <Col key={index} style={{ margin: '12px 0' }}><MenuCard loading={loading} /></Col>)
+        }
+      </Row>
+    );
+  }
+}
 
 export default MenuCardGrid;
