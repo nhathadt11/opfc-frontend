1158edeb12de86df16e1a0f8fda6d3a41630e170
Nhat Ha <nhathadt11@gmail.com>
2018-09-28 22:44:29 +0700

    Add content tab header

--- src/modules/EventPlanner/EventPlanner.styled.js
+++ src/modules/EventPlanner/EventPlanner.styled.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const EventPlannerTabContentStyled = styled.div`
-  padding: 30px 20px;
+  padding: 0 30px 20px;
   width: 100%;
+`;
+
+export const EventPlannerTabTitleStyled = styled.h2`
+  margin-bottom: 20px;
 `;
--- src/modules/EventPlanner/components/EventPlannerAccount/EventPlannerAccount.js
+++ src/modules/EventPlanner/components/EventPlannerAccount/EventPlannerAccount.js
@@ -4,6 +4,7 @@ import {
   Form, Input, Cascader, Select, Button,
 } from 'antd';
 import './EventPlannerAccount.css';
+import { EventPlannerTabTitleStyled } from '../../EventPlanner.styled';
 
 const FormItem = Form.Item;
 const { Option } = Select;
@@ -74,86 +75,89 @@ class EventPlannerAccount extends Component {
     );
 
     return (
-      <Form className="opfc-event-planner-account">
-        <FormItem
-          {...formItemLayout}
-          label="Username"
-        >
-          {getFieldDecorator('username', {
-            rules: [{
-              required: true, message: 'Please input your Username!',
-            }],
-          })(
-            <Input />,
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="E-mail"
-        >
-          {getFieldDecorator('email', {
-            rules: [{
-              type: 'email', message: 'The input is not valid E-mail!',
-            }, {
-              required: true, message: 'Please input your E-mail!',
-            }],
-          })(
-            <Input />,
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="Password"
-        >
-          {getFieldDecorator('password', {
-            rules: [{
-              required: true, message: 'Please input your password!',
-            }, {
-              validator: this.validateToNextPassword,
-            }],
-          })(
-            <Input type="password" />,
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="Confirm Password"
-        >
-          {getFieldDecorator('confirm', {
-            rules: [{
-              required: true, message: 'Please confirm your password!',
-            }, {
-              validator: this.compareToFirstPassword,
-            }],
-          })(
-            <Input type="password" onBlur={this.handleConfirmBlur} />,
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="City/District/Ward"
-        >
-          {getFieldDecorator('residence', {
-            initialValue: ['zhejiang', 'hangzhou', 'xihu'],
-            rules: [{ type: 'array', required: true, message: 'Please select your City/District/Ward!' }],
-          })(
-            <Cascader options={residences} />,
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="Phone Number"
-        >
-          {getFieldDecorator('phone', {
-            rules: [{ required: true, message: 'Please input your phone number!' }],
-          })(
-            <Input addonBefore={prefixSelector} style={{ width: '100%' }} />,
-          )}
-        </FormItem>
-        <FormItem {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">Save</Button>
-        </FormItem>
-      </Form>
+      <div>
+        <EventPlannerTabTitleStyled>Account</EventPlannerTabTitleStyled>
+        <Form className="opfc-event-planner-account">
+          <FormItem
+            {...formItemLayout}
+            label="Username"
+          >
+            {getFieldDecorator('username', {
+              rules: [{
+                required: true, message: 'Please input your Username!',
+              }],
+            })(
+              <Input />,
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="E-mail"
+          >
+            {getFieldDecorator('email', {
+              rules: [{
+                type: 'email', message: 'The input is not valid E-mail!',
+              }, {
+                required: true, message: 'Please input your E-mail!',
+              }],
+            })(
+              <Input />,
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="Password"
+          >
+            {getFieldDecorator('password', {
+              rules: [{
+                required: true, message: 'Please input your password!',
+              }, {
+                validator: this.validateToNextPassword,
+              }],
+            })(
+              <Input type="password" />,
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="Confirm Password"
+          >
+            {getFieldDecorator('confirm', {
+              rules: [{
+                required: true, message: 'Please confirm your password!',
+              }, {
+                validator: this.compareToFirstPassword,
+              }],
+            })(
+              <Input type="password" onBlur={this.handleConfirmBlur} />,
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="City/District/Ward"
+          >
+            {getFieldDecorator('residence', {
+              initialValue: ['zhejiang', 'hangzhou', 'xihu'],
+              rules: [{ type: 'array', required: true, message: 'Please select your City/District/Ward!' }],
+            })(
+              <Cascader options={residences} />,
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="Phone Number"
+          >
+            {getFieldDecorator('phone', {
+              rules: [{ required: true, message: 'Please input your phone number!' }],
+            })(
+              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />,
+            )}
+          </FormItem>
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">Save</Button>
+          </FormItem>
+        </Form>
+      </div>
     );
   }
 }
