33b0a56a9404658448be19a4f9981393529973a3
Nhat Ha <nhathadt11@gmail.com>
2018-10-15 17:27:22 +0700

    Display event info in step picking menus

--- src/modules/EventPlanner/components/EventPlanningFlow/EventPlanningSteps/StepPickMenuSider.js
+++ src/modules/EventPlanner/components/EventPlanningFlow/EventPlanningSteps/StepPickMenuSider.js
@@ -1,36 +1,68 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import {
+  shape, arrayOf, number, string,
+} from 'prop-types';
+import { find } from 'lodash';
+import moment from 'moment';
 import {
   SiderItemTitleStyled, SiderItemValueStyled,
   StepPickMenuSiderStyled, StepPickMenuSiderItemStyled,
 } from './StepMenuSider.styled';
 
-const StepPickMenuSider = () => (
-  <StepPickMenuSiderStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Event Name</SiderItemTitleStyled>
-      <SiderItemValueStyled>ABC</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Event Type</SiderItemTitleStyled>
-      <SiderItemValueStyled>Birthday</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Budget</SiderItemTitleStyled>
-      <SiderItemValueStyled>$23.60</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Serving Number</SiderItemTitleStyled>
-      <SiderItemValueStyled>12</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Time Range</SiderItemTitleStyled>
-      <SiderItemValueStyled>Sept 12th, 10:00 pm - 12:00pm</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-    <StepPickMenuSiderItemStyled>
-      <SiderItemTitleStyled>Address</SiderItemTitleStyled>
-      <SiderItemValueStyled>123 New York</SiderItemValueStyled>
-    </StepPickMenuSiderItemStyled>
-  </StepPickMenuSiderStyled>
-);
+const StepPickMenuSider = ({ selectedEvent, eventTypeList }) => {
+  const foundEvent = find(eventTypeList, e => e.id === selectedEvent.eventTypeId);
 
-export default StepPickMenuSider;
+  return (
+    <StepPickMenuSiderStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Event Name</SiderItemTitleStyled>
+        <SiderItemValueStyled>{selectedEvent.eventName}</SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Event Type</SiderItemTitleStyled>
+        <SiderItemValueStyled>
+          {
+            foundEvent ? foundEvent.eventTypeName : 'N/A'
+          }
+        </SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Budget</SiderItemTitleStyled>
+        <SiderItemValueStyled>{selectedEvent.budget}</SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Serving Number</SiderItemTitleStyled>
+        <SiderItemValueStyled>{selectedEvent.servingNumber}</SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Time Range</SiderItemTitleStyled>
+        <SiderItemValueStyled>
+          { moment(selectedEvent.startAt).format('HH:mm A') } - { moment(selectedEvent.endAt).format('HH:mm A') }
+        </SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+      <StepPickMenuSiderItemStyled>
+        <SiderItemTitleStyled>Address</SiderItemTitleStyled>
+        <SiderItemValueStyled>{selectedEvent.address}</SiderItemValueStyled>
+      </StepPickMenuSiderItemStyled>
+    </StepPickMenuSiderStyled>
+  );
+};
+
+StepPickMenuSider.propTypes = {
+  selectedEvent: shape({}).isRequired,
+  eventTypeList: arrayOf(shape({
+    id: number,
+    eventTypeName: string,
+  })).isRequired,
+};
+
+const mapStateToProps = state => ({
+  selectedEvent: state.eventPlannerReducer.event.event,
+  eventTypeList: state.generalReducer.eventTypeList,
+});
+
+export default compose(
+  connect(mapStateToProps),
+)(StepPickMenuSider);
