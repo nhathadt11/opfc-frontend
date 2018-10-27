098215b8460db10c582f34bd5058bc9c80c1ce48
Nhat Ha <nhathadt11@gmail.com>
2018-10-27 16:41:29 +0700

    Apply style for menu card grid

--- src/containers/MenuCardGrid/MenuCardGrid.js
+++ src/containers/MenuCardGrid/MenuCardGrid.js
@@ -3,9 +3,10 @@ import { map } from 'lodash';
 import { Row, Col } from 'antd';
 import { arrayOf, shape } from 'prop-types';
 import MenuCard from '../../components/MenuCard/MenuCard';
+import './MenuCardGrid.css';
 
 const MenuCardGrid = ({ dataList }) => (
-  <Row type="flex" gutter={24}>
+  <Row type="flex" gutter={24} className="opfc-menu-card-grid">
     {
       map(dataList, (item, index) => <Col key={index} style={{ margin: '12px 0' }}><MenuCard menu={item} /></Col>)
     }
