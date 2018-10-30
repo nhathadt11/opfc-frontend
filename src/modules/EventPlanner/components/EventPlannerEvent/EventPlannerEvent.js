99504fc584a437f4047825ffbf95d71753e244b0
Nhat Ha <nhathadt11@gmail.com>
2018-10-30 15:34:46 +0700

    Loading indicator for event list

--- src/modules/EventPlanner/components/EventPlannerEvent/EventPlannerEvent.js
+++ src/modules/EventPlanner/components/EventPlannerEvent/EventPlannerEvent.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {
-  Row, Col, Button, Icon,
+  Row, Col, Button, Icon, Spin,
 } from 'antd';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import {
-  arrayOf, shape, number, func,
+  arrayOf, shape, number, func, bool,
 } from 'prop-types';
 import { map } from 'lodash';
 import { EventPlannerTabTitleStyled } from '../../EventPlanner.styled';
@@ -25,6 +25,7 @@ class EventPlannerEvent extends Component {
       push: func.isRequired,
     }).isRequired,
     deselectEventAction: func.isRequired,
+    fetching: bool.isRequired,
   }
 
   componentDidMount() {
@@ -33,31 +34,35 @@ class EventPlannerEvent extends Component {
   }
 
   render() {
-    const { eventList, history: { push }, deselectEventAction } = this.props;
+    const {
+      eventList, history: { push }, deselectEventAction, fetching,
+    } = this.props;
 
     return (
       <div>
         <EventPlannerTabTitleStyled>Event</EventPlannerTabTitleStyled>
-        <Row type="flex" gutter={24}>
-          {
-            map(eventList, (event => (
-              <Col key={event.id} md={24} lg={12} xl={8} className="opfc-event-card"><EventCard data={event} /></Col>
-            )))
-          }
-          {
-            <Col key="event-card-create" md={24} lg={12} xl={8} className="opfc-event-card">
-              <Button
-                className="opfc-event-card-create"
-                onClick={() => {
-                  push('/profile/event-planner/event/create');
-                  deselectEventAction();
-                }}
-              >
-                <Icon type="plus" />
-              </Button>
-            </Col>
-          }
-        </Row>
+        <Spin spinning={fetching}>
+          <Row type="flex" gutter={24}>
+            {
+              map(eventList, (event => (
+                <Col key={event.id} md={24} lg={12} xl={8} className="opfc-event-card"><EventCard data={event} /></Col>
+              )))
+            }
+            {
+              <Col key="event-card-create" md={24} lg={12} xl={8} className="opfc-event-card">
+                <Button
+                  className="opfc-event-card-create"
+                  onClick={() => {
+                    push('/profile/event-planner/event/create');
+                    deselectEventAction();
+                  }}
+                >
+                  <Icon type="plus" />
+                </Button>
+              </Col>
+            }
+          </Row>
+        </Spin>
       </div>
     );
   }
@@ -65,6 +70,7 @@ class EventPlannerEvent extends Component {
 
 const mapStateToProps = state => ({
   eventList: state.eventPlannerReducer.event.eventList,
+  fetching: state.eventPlannerReducer.event.fetching,
 });
 
 const mapDispatchToProps = {
