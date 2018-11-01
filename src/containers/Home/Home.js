9f0f223f7d9795da82235c7f454b5fbfaa167550
Nhat Ha <nhathadt11@gmail.com>
2018-11-01 12:35:55 +0700

    Fix incorrect search after navigating back to homepage

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
 
 const mapStateToProps = state => ({
   menuList: state.generalReducer.menuList,
-  fullTextSearchValue: state.generalReducer.fullTextSearch.value,
+  fullTextSearchValue: state.generalReducer.fullTextSearchValue,
 });
 
 const mapDispatchToProps = {
