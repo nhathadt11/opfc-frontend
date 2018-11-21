231848431d8a393674dbe6ef3d24e6c59a4e2153
Nhat Ha <nhathadt11@gmail.com>
2018-11-21 21:28:03 +0700

    Add no menu found component

--- src/containers/MenuCardGrid/MenuCardGrid.js
+++ src/containers/MenuCardGrid/MenuCardGrid.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { map } from 'lodash';
+import { map, isEmpty } from 'lodash';
 import { Row, Col, Spin } from 'antd';
 import { arrayOf, shape, bool } from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import MenuCard from '../../components/MenuCard/MenuCard';
 import './MenuCardGrid.css';
+import LocalIcon from '../../fonts/LocalFont';
+
+const NoMenuFound = (
+  <div className="opfc-no-menu-found">
+    <LocalIcon type="icon-menu" />
+    <section>Sorry! No menu found.</section>
+  </div>
+);
 
 const MenuCardGrid = ({ dataList, fetching, forceFetching }) => (
   <Spin spinning={forceFetching || fetching}>
     <Row type="flex" gutter={24} className="opfc-menu-card-grid">
       {
         map(dataList, (item, index) => <Col key={index} style={{ margin: '12px 0' }}><MenuCard menu={item} /></Col>)
+      }
+      {
+        !fetching && !forceFetching && isEmpty(dataList) && NoMenuFound
       }
     </Row>
   </Spin>
